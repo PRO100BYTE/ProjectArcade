2cfc06b0fd85ef82797c9bb850dc7e70e7b8b598
TheDayG0ne <thedayg0ne@internet.ru>
2023-04-07 15:20:15 +0700

    Update RetroBat sources

--- source/build.bat
+++ source/build.bat
@@ -39,7 +39,7 @@ set archive_compression=3
 
 :: ---- LOOP LIST ----
 
-set deps_list=(git makensis 7za strip wget)
+set deps_list=( makensis 7za strip wget)
 set submodules_list=(bios default_theme decorations system)
 set packages_list=(retrobat_binaries batgui emulationstation batocera_ports mega_bezels retroarch roms wiimotegun)
 set legacy_cores_list=(4do emuscv imageviewer mame2014 mame2016 px68k)
