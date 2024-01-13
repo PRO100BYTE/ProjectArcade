f94f2f99bd10c3b4fe8b493296055bb543680016
Evgeniy Struchkov <thedayg0ne@internet.ru>
2024-01-13 20:13:01 +0700

    Upload ArcadeGUI sketches
    Co-Authored-By: PRO100BYTE Helper <helper@pro100byte.ru>

---
+++ pa_gui/ArcadeGUI/Bkp_V24_20240113_1824/README.md
@@ -0,0 +1,9 @@
+# ProjectArcade Settings GUI
+Settings GUI for ProjectArcade
+ 
+- GUI to help managing ProjectArcade config file
+- Quick gamelist editor
+- Import Steam Game in ProjectArcade
+- Playlist creator for Xbox 360 and PS3 Games
+ 
+Made with WinDev 24
---
+++ pa_gui/ArcadeGUI/Data/YourSteamGame.bat
@@ -0,0 +1,15 @@
+@echo OFF
+
+REM HERE MODIFY YOUR STEAM APPID:
+START steam://rungameid/000000
+
+TIMEOUT /t 30
+:RUNNING
+
+REM HERE YOU MUST ENTER THE RIGHT NAME OF THE EXECUTABLE, FOR EXAMPLE: "MyGame.exe"
+tasklist|findstr "Mygame.exe" > nul
+
+if %errorlevel%==1 timeout /t 5 & taskkill /F /IM Steam.exe /T & GOTO ENDLOOP
+timeout /t 2
+GOTO RUNNING
+:ENDLOOP
