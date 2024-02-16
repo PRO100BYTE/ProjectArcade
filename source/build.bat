ad7ea9a7ddaee21351b2fc2726175c2c1ff4d69f
Evgeniy Struchkov <thedayg0ne@internet.ru>
2024-02-16 12:10:25 +0700

    Update RetroBat sources

--- source/build.bat
+++ source/build.bat
@@ -43,15 +43,15 @@ set setup_compiler=ISCC
 set deps_list=(git !setup_compiler! 7za wget curl)
 set submodules_list=(bios decorations system)
 set packages_list=(retrobat_binaries batgui emulationstation default_theme batocera_ports mega_bezels retroarch roms wiimotegun)
-set legacy_cores_list=(4do emuscv hatarib imageviewer mame2014 mame2016)
-set emulators_black_list=(3dsen pico8 retroarch ryujinx steam teknoparrot yuzu yuzu-early-access)
+set legacy_cores_list=(4do emuscv fake08 hatarib imageviewer mame2014 mame2016)
+set emulators_black_list=(3dsen magicengine pico8 retroarch ryujinx steam teknoparrot yuzu yuzu-early-access)
 
 :: ---- GET STARTED ----
 
 set script_type=builder
 set user_choice=0
 set git_branch=master
-set branch=beta
+set branch=stable
 set release_version=null
 set log_file=build.log
 set exit_timeout=0
@@ -382,7 +382,7 @@ for %%i in %packages_list% do (
 		
 		call :download
 		call :hash_file
-		call :extract		
+		call :extract
 	)
 )
 
---
+++ source/launcher/README.md
@@ -0,0 +1,17 @@
+# RetroBat Launcher (MFA)
+
+You need Clickteam Fusion 2.5 Developer updated to the latest release to open mfa file and build retrobat.exe.
+
+This launcher has been created with a proprietary software and is aimed to be replaced by a full new open source launcher in the future.
+
+# License
+
+Copyright (C) Adrien Chalard
+
+[![CC BY-NC-SA 4.0][cc-by-nc-sa-shield]][cc-by-nc-sa]
+
+[![CC BY-NC-SA 4.0][cc-by-nc-sa-image]][cc-by-nc-sa]
+
+[cc-by-nc-sa]: http://creativecommons.org/licenses/by-nc-sa/4.0/
+[cc-by-nc-sa-image]: https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png
+[cc-by-nc-sa-shield]: https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-lightgrey.svg
